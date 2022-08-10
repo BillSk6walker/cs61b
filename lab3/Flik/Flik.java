e128fad95dfcbeefe3c178a2b845e40f29a4be0f
Bill <3210105508@zju.edu.cn>
2022-08-10 14:30:26 +0800

    lab3 done

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,21 @@
-/** An Integer tester created by Flik Enterprises. */
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+/**
+ * An Integer tester created by Flik Enterprises.
+ */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
         return a == b;
+    }
+
+    @Test
+    public void TestIsSameNumber() {
+        int a = 128;
+        int b = 128;
+        assertTrue(isSameNumber(a, b));
     }
 }
